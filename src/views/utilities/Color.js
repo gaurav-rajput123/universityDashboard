3bc6983b7dfa5c7e4acdd00de3088a571a5682b0
Vikram <vikrambahadur0022@gmail.com>
2022-05-31 15:29:17 +0530

    pull

--- src/views/utilities/Color.js
+++ src/views/utilities/Color.js
@@ -45,15 +45,15 @@ const columns = [
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+    { id: 1, lastName: 'Kumari', firstName: 'Neha', age: 35 },
+    { id: 2, lastName: 'Sharma', firstName: 'Lata', age: 42 },
+    { id: 3, lastName: 'Kaur', firstName: 'Jasmine', age: 45 },
+    { id: 4, lastName: 'khatri', firstName: 'Anchal', age: 16 },
+    { id: 5, lastName: 'Rawat', firstName: 'Sakshi', age: null },
+    { id: 6, lastName: 'Sharma', firstName: 'Srijana', age: 150 },
+    { id: 7, lastName: 'Kaur', firstName: 'Simran', age: 44 },
+    { id: 8, lastName: 'Saluja', firstName: 'Roji', age: 36 },
+    { id: 9, lastName: 'Kumari', firstName: 'Sita', age: 65 }
 ];
 
 const ColorBox = ({ bgcolor, title, data, dark }) => (
--- src/views/utilities/Typography.js
+++ src/views/utilities/Typography.js
@@ -42,15 +42,15 @@ const columns = [
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+    { id: 1, lastName: 'Bahadur', firstName: 'Vikram', age: 35 },
+    { id: 2, lastName: 'Mandhan', firstName: 'Sohit', age: 42 },
+    { id: 3, lastName: 'Rajput', firstName: 'Gaurav', age: 45 },
+    { id: 4, lastName: 'Sharma', firstName: 'Vineet', age: 16 },
+    { id: 5, lastName: 'Kumar', firstName: 'Prashant', age: 56 },
+    { id: 6, lastName: 'Sandhu', firstName: 'Balkar', age: 150 },
+    { id: 7, lastName: 'Kumar', firstName: 'Ankit', age: 44 },
+    { id: 8, lastName: 'Panwar', firstName: 'Ankur', age: 36 },
+    { id: 9, lastName: 'Bahadur', firstName: 'Vikas', age: 65 }
 ];
 
 const Typography = () => (
