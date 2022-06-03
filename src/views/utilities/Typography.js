94bf17e3f80987b1e9e246ab2efa07405e4340ec
Gaurav <rajputgr513gssm@gmail.com>
2022-06-03 12:31:05 +0530

    AS

--- src/views/utilities/Typography.js
+++ src/views/utilities/Typography.js
@@ -7,56 +7,80 @@ import MainCard from 'ui-component/cards/MainCard';
 import SecondaryAction from 'ui-component/cards/CardSecondaryAction';
 import { gridSpacing } from 'store/constant';
 import { DataGrid } from '@mui/x-data-grid';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { countContext } from 'index';
+import axios from 'axios';
 // ==============================|| TYPOGRAPHY ||============================== //
 
 const columns = [
     {
-        field: 'id',
-        headerName: 'ID',
+        field: 'userId',
+        headerName: 'userId',
         width: 70
     },
     {
-        field: 'firstName',
-        headerName: 'First name',
-        width: 130
+        field: 'courseId',
+        headerName: 'courseId',
+        width: 350
     },
-    {
-        field: 'lastName',
-        headerName: 'Last name',
-        width: 130
-    },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90
-    },
-    {
-        field: 'Email ID',
-        headerName: 'Email ID',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`
-    }
+    // {
+    //     field: 'firstName',
+    //     headerName: 'First name',
+    //     width: 130
+    // },
+    // {
+    //     field: 'lastName',
+    //     headerName: 'Last name',
+    //     width: 130
+    // },
+    
+    // {
+    //     field: 'Email ID',
+    //     headerName: 'Email ID',
+    //     description: 'This column has a value getter and is not sortable.',
+    //     sortable: false,
+    //     width: 160,
+    //     valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`
+    // }
 ];
 
-const rows = [
-    { id: 1, lastName: 'Bahadur', firstName: 'Vikram', age: 35 },
-    { id: 2, lastName: 'Mandhan', firstName: 'Sohit', age: 42 },
-    { id: 3, lastName: 'Rajput', firstName: 'Gaurav', age: 45 },
-    { id: 4, lastName: 'Sharma', firstName: 'Vineet', age: 16 },
-    { id: 5, lastName: 'Kumar', firstName: 'Prashant', age: 56 },
-    { id: 6, lastName: 'Sandhu', firstName: 'Balkar', age: 150 },
-    { id: 7, lastName: 'Kumar', firstName: 'Ankit', age: 44 },
-    { id: 8, lastName: 'Panwar', firstName: 'Ankur', age: 36 },
-    { id: 9, lastName: 'Bahadur', firstName: 'Vikas', age: 65 }
-];
+// const rows = [
+//     { id: 1, lastName: 'Bahadur', firstName: 'Vikram', age: 35 },
+//     { id: 2, lastName: 'Mandhan', firstName: 'Sohit', age: 42 },
+//     { id: 3, lastName: 'Rajput', firstName: 'Gaurav', age: 45 },
+//     { id: 4, lastName: 'Sharma', firstName: 'Vineet', age: 16 },
+//     { id: 5, lastName: 'Kumar', firstName: 'Prashant', age: 56 },
+//     { id: 6, lastName: 'Sandhu', firstName: 'Balkar', age: 150 },
+//     { id: 7, lastName: 'Kumar', firstName: 'Ankit', age: 44 },
+//     { id: 8, lastName: 'Panwar', firstName: 'Ankur', age: 36 },
+//     { id: 9, lastName: 'Bahadur', firstName: 'Vikas', age: 65 }
+// ];
 
 const Typography = () => {
     let context = useContext(countContext)
+
+    const [rows, setRows] = useState([])
+    useEffect(()=>{
+        async function getTeachers () {
+            try {
+                const callRes = await axios({
+                    url: "https://api.keewesolutions.com/university/studentlist", method: 'GET'
+                })
+                const dataRowArr = callRes.data.response.map((item)=>{
+                    return {
+                        userId: item.id,
+                        courseId: item.courseId,
+                        id: item.id + item.courseId
+                    }
+                })
+                console.log(dataRowArr)
+                setRows(dataRowArr)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getTeachers()
+    },[])
     return (
 
     <MainCard title="Student Status" 
@@ -72,7 +96,7 @@ const Typography = () => {
         <Grid container spacing={gridSpacing}>
             <Grid item xs={12} sm={12}>
                 <div style={{ height: 400, width: '100%' }}>
-                    <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
+                    <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[39]} checkboxSelection />
                 </div>
                 {/* <Grid container direction="column" spacing={1}>
                         <Grid item>
