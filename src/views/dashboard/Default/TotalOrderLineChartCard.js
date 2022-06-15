36841c1fe617f91ea6826960f88e869e326768c4
sohit <sohitmandhan12@gmail.com>
2022-06-15 09:41:13 +0530

    ronit

--- src/views/dashboard/Default/TotalOrderLineChartCard.js
+++ src/views/dashboard/Default/TotalOrderLineChartCard.js
@@ -128,7 +128,7 @@ const TotalOrderLineChartCard = ({ isLoading }) => {
                                                     </Typography>
                                                 ) : (
                                                     <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>
-                                                        Rs 5
+                                                        Rs 8
                                                     </Typography>
                                                 )}
                                             </Grid>
